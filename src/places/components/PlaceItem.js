c18ba5919608ca77037d82a23a66d9e8dec3daf6
Liana <82238565+Li-create2021@users.noreply.github.com>
2021-12-28 16:31:43 +0100

    Added custom buttons

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/FormElements/Button";
 import "./PlaceItem.css";
 
 const PlaceItem = (props) => {
@@ -18,9 +19,9 @@ const PlaceItem = (props) => {
         </div>
         {/* </Link> */}
         <div className="place-item__actions">
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/place/${props.id}`}>EDIT</Button>
+          <Button danger>DELETE</Button>
         </div>
       </Card>
     </li>
