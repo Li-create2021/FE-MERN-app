8fb7862115fa2572ebe76652c332ea5fd52b3644
Liana <82238565+Li-create2021@users.noreply.github.com>
2021-12-28 13:42:48 +0100

    used card component

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,19 +1,23 @@
 import React from "react";
+
 import UserItem from "./UserItem";
+import Card from "../../shared/UIElements/components/Card";
 import "./UsersList.css";
 
 const UsersList = (props) => {
-  if (props.item.length === 0) {
+  if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
-  return;
-  <ul className="users-list">
-    {props.item.map((user) => {
-      return (
+
+  return (
+    <ul className="users-list">
+      {props.items.map((user) => (
         <UserItem
           key={user.id}
           id={user.id}
@@ -21,9 +25,9 @@ const UsersList = (props) => {
           name={user.name}
           placeCount={user.places}
         />
-      );
-    })}
-  </ul>;
+      ))}
+    </ul>
+  );
 };
 
 export default UsersList;
