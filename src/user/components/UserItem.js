6be04124d3f5b9042abb7fe775f630bc877b7fed
Liana <82238565+Li-create2021@users.noreply.github.com>
2021-12-28 12:07:19 +0100

    Added a Link on the user card

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,20 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-import "./UsersList.css";
+import Avatar from "../../shared/UIElements/components/Avatar";
+import "./UserItem.css";
 
 const UserItem = (props) => {
   return (
     <li className="user-item">
       <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={props.image} alt={props.name} />
+        <Link to={`${props.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.alt} />
+          </div>
           <div className="user-item__info">
             <h2>{props.name}</h2>
             <h3>
               {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
             </h3>
           </div>
-        </div>
+        </Link>
       </div>
     </li>
   );
