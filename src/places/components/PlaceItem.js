16ca4ffe6f365a08eb382a84f2fd424b83deda36
Liana <82238565+Li-create2021@users.noreply.github.com>
2021-12-29 11:44:07 +0100

    Rendered a map with Google Maps

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -3,21 +3,18 @@ import React, { useState } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
 import "./PlaceItem.css";
 
 const PlaceItem = (props) => {
   const [showMap, setShowMap] = useState(false);
 
-  const openMapHandler = () => {
-    setShowMap(true);
-  };
+  const openMapHandler = () => setShowMap(true);
 
-  const closeMapHandler = () => {
-    setShowMap(false);
-  };
+  const closeMapHandler = () => setShowMap(false);
 
   return (
-    <>
+    <React.Fragment>
       <Modal
         show={showMap}
         onCancel={closeMapHandler}
@@ -26,10 +23,10 @@ const PlaceItem = (props) => {
         footerClass="place-item__modal-actions"
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
-        <div className="map-container"></div>
-        <h2>THE MAP</h2>
+        <div className="map-container">
+          <Map center={props.coordinates} zoom={16} />
+        </div>
       </Modal>
-
       <li className="place-item">
         <Card className="place-item__content">
           <div className="place-item__image">
@@ -40,7 +37,6 @@ const PlaceItem = (props) => {
             <h3>{props.address}</h3>
             <p>{props.description}</p>
           </div>
-
           <div className="place-item__actions">
             <Button inverse onClick={openMapHandler}>
               VIEW ON MAP
@@ -50,7 +46,7 @@ const PlaceItem = (props) => {
           </div>
         </Card>
       </li>
-    </>
+    </React.Fragment>
   );
 };
 
