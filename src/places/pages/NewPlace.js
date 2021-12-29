dfe0c93e3309d45a2ac167950b43e75c3c02fad7
Liana <82238565+Li-create2021@users.noreply.github.com>
2021-12-29 15:57:57 +0100

    Shared input values and added more inputs on the form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import { VALIDATOR_MINLENGTH } from "../../shared/util/validators";
 import Input from "../../shared/FormElements/Input";
 import "./NewPlace.css";
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="place-form">
       <Input
@@ -13,6 +18,16 @@ const NewPlace = () => {
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        type="text"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={descriptionInputHandler}
       />
     </form>
   );
